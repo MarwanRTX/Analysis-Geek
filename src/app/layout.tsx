ec998d42eeacf48b306c140bf95fbef86147afcb
Marwan Mostafa <114308810+MarwanRTX@users.noreply.github.com>
2024-11-10 23:57:14 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,27 @@
 // app/layout.tsx
-import Navbar from "./components/Navbar";
+"use client"; // Ensure this is marked as a client component
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+import { ThemeProvider } from 'next-themes';
+import Navbar from './components/Navbar';
+import './globals.css'; 
+import { useEffect, useState } from 'react';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <html lang="en">
-      <body>
-        <Navbar />
-        {children}
+      <body className="transition-colors duration-300">
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
